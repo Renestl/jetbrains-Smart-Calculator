7629b437a11f0165acda61ef93ba5c141f792f06
Jennifer Currie <renestl@gmail.com>
2020-10-13 00:27:00 -0500

    Calculate the sum of two integers

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,1 +1,11 @@
 # write your code here
+class Calculator:
+
+    def addition(self):
+        a, b = input().split(" ")
+        print(int(a) + int(b))
+
+
+if __name__ == '__main__':
+    calculator = Calculator()
+    calculator.addition()
